3a6592e6f53cb68a0b66fa9d1fe4cfb527caee19
ludovic_gendre <ludovic.gendre@g-mail.fr>
2020-10-26 09:49:39 +0100

    Ajout Background en fonction du lvl choisi

--- lib/views/view-playing.dart
+++ lib/views/view-playing.dart
@@ -1,16 +1,23 @@
 import 'package:flame/sprite.dart';
+import 'package:rainofwords/components/btn_level.dart';
 import 'package:rainofwords/game_controller.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
 class PlayingView {
   final GameController game;
+  BtnLevel btnLevel;
   Rect homeRect;
   Sprite homeSprite;
 
   PlayingView(this.game) {
     resize();
-    homeSprite = Sprite('Backg/blue_mountain.png');
+    // ignore: unrelated_type_equality_checks
+    if (btnLevel == true) {
+      homeSprite = Sprite('Backg/blue_mountain.png');
+    } else {
+      homeSprite = Sprite('Backg/night_mountain.png');
+    }
   }
 
   void render(Canvas c) {
@@ -20,7 +27,7 @@ class PlayingView {
   void update(double t) {}
 
   void resize() {
-    homeRect =
-        Rect.fromLTWH(0, 0, game.screenSize.width, game.screenSize.height);
+    homeRect = Rect.fromLTWH(
+        0, 0, game.screenSize.width, game.screenSize.height / 1.5);
   }
 }
